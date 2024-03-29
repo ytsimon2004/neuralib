53c3d2d2d9c1107727400c5b67474bac5abfb2da
Yu-Ting Wei <ytsimon2004@gmail.com>
2024-03-29 18:55:57 +0100

    add brender app

---
+++ src/neuralib/atlas/brainrender/main_app.py
@@ -0,0 +1,26 @@
+from neuralib.argp import parse_command_args
+from neuralib.atlas.brainrender import BrainReconstructor, RoisReconstructor, ProbeReconstructor
+
+
+def main():
+    parsers = dict(
+        area=BrainReconstructor,
+        roi=RoisReconstructor,
+        probe=ProbeReconstructor
+    )
+
+    parse_command_args(
+        parsers=parsers,
+        description='brainrender view',
+        usage="""
+        Example: 
+        >> brender area -R VISal,VISam,VISl,VISli,VISp,VISpl,VISpm,VISpor
+        >> brender roi -F <FILE>
+        >> brender probe -F <FILE> --depth <DEPTH in um>
+        
+        """
+    )
+
+
+if __name__ == '__main__':
+    main()
