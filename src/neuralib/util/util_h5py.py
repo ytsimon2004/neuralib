c77b34b71108e8b8fd394933116fa4e94fdb55e7
Ta-Shun Su <antoniost29@gmail.com>
2024-11-20 17:46:41 +0100

    add util_h5py

---
+++ src/neuralib/util/util_h5py.py
@@ -0,0 +1,214 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Literal, get_type_hints, overload
+
+import h5py
+import numpy as np
+
+__all__ = [
+    'H5pyDataWrapper', 'attr', 'group', 'array'
+]
+
+OPEN = Literal['r', 'r+', 'w', 'x', 'a']
+
+
+class H5pyDataWrapper:
+    def __init__(self, file: str | Path | h5py.File | h5py.Group,
+                 mode: OPEN = 'r'):
+        """
+
+        :param file: file path
+        """
+
+        if isinstance(file, Path):
+            file = str(file)
+
+        if isinstance(file, str):
+            file = h5py.File(file, mode)
+
+        self.__file = file
+
+    @property
+    def file(self) -> h5py.File | h5py.Group:
+        return self.__file
+
+    def __del__(self):
+        if isinstance(file := self.__file, h5py.File):
+            file.close()
+
+    def __getitem__(self, item):
+        return self.__file[item]
+
+
+def attr(name: str = None):
+    return H5pyDataWrapperAttr(name)
+
+
+def group(name: str = None):
+    return H5pyDataWrapperGroup(name)
+
+
+@overload
+def array(key: str = None,
+          chunks: bool | int | tuple[int, ...] = None,
+          maxshape: int | tuple[int, ...] = None,
+          compression: int | str = None,
+          compression_opts: int | tuple = None,
+          scaleoffset: int = None,
+          shuffle: bool = None,
+          fillvalue: int | float | str = None,
+          **kwargs) -> np.ndarray:
+    pass
+
+
+def array(key: str = None, **kwargs) -> np.ndarray:
+    return H5pyDataWrapperArray(key, **kwargs)
+
+
+class H5pyDataWrapperAttr:
+    __slots__ = '__attr', '__type'
+
+    def __init__(self, name: str = None):
+        self.__attr = name
+        self.__type = None
+
+    def __set_name__(self, owner, name):
+        if not issubclass(owner, H5pyDataWrapper):
+            raise TypeError('owner type not H5pyDataWrapper')
+
+        if self.__attr is None:
+            self.__attr = name
+        self.__type = get_type_hints(owner).get(name, None)
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            file: h5py.File = instance.file
+
+            try:
+                ret = file.attrs[self.__attr]
+            except KeyError as e:
+                raise AttributeError(self.__attr) from e
+
+            if self.__type is not None:
+                ret = self.__type(ret)
+            return ret
+
+    def __set__(self, instance, value):
+        try:
+            instance.file.attrs[self.__attr] = value
+        except OSError as e:
+            raise AttributeError(self.__attr) from e
+
+    def __delete__(self, instance):
+        try:
+            del instance.file.attrs[self.__attr]
+        except OSError as e:
+            raise AttributeError(self.__attr) from e
+
+
+class H5pyDataWrapperGroup:
+    __slots__ = '__group', '__type'
+
+    def __init__(self, group: str = None):
+        self.__group = group
+        self.__type = None
+
+    def __set_name__(self, owner, name):
+        if not issubclass(owner, H5pyDataWrapper):
+            raise TypeError('owner type not H5pyDataWrapper')
+
+        if self.__group is None:
+            self.__group = name
+
+        self.__type = get_type_hints(owner).get(name, None)
+        if self.__type is not None and not issubclass(self.__type, H5pyDataWrapper):
+            raise TypeError(f'h5py_group {name} type not H5pyDataWrapper')
+        if self.__type is None:
+            self.__type = H5pyDataWrapper
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            file: h5py.File = instance.file
+
+            try:
+                return self.__type(file[self.__group])
+            except KeyError:
+                pass
+
+            try:
+                return self.__type(file.create_group(self.__group))
+            except ValueError as e:
+                raise AttributeError(self.__group) from e
+
+
+class H5pyDataWrapperArray:
+    __slots__ = '__key', '__kwargs'
+
+    def __init__(self, key: str = None, **kwargs):
+        self.__key = key
+        self.__kwargs = kwargs
+
+    def __set_name__(self, owner, name):
+        if not issubclass(owner, H5pyDataWrapper):
+            raise TypeError('owner type not H5pyDataWrapper')
+
+        if self.__key is None:
+            self.__key = name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            file: h5py.File = instance.file
+
+            try:
+                ret = file[self.__key]
+            except KeyError:
+                pass
+            else:
+                return H5pyDataWrapperLazyArray(ret)
+
+            return None
+
+    def __set__(self, instance, value: np.ndarray):
+        file: h5py.File = instance.file
+        try:
+            file[self.__key]
+        except KeyError:
+            pass
+        else:
+            del file[self.__key]
+
+        file.create_dataset(self.__key, data=value, **self.__kwargs)
+
+    def __delete__(self, instance):
+        file: h5py.File = instance.file
+        del file[self.__key]
+
+
+class H5pyDataWrapperLazyArray:
+    def __init__(self, data: h5py.Dataset):
+        self.__data = data
+
+    @property
+    def ndim(self):
+        return self.__data.ndim
+
+    @property
+    def dtype(self):
+        return self.__data.dtype
+
+    @property
+    def shape(self):
+        return self.__data.shape
+
+    def __array__(self, *args, **kwargs):
+        return np.asarray(self.__data, *args, **kwargs)
+
+    def __getitem__(self, item):
+        return np.asarray(self.__data[item])
---
+++ test/_unittest/test_h5py.py
@@ -0,0 +1,99 @@
+import math
+import unittest
+from pathlib import Path
+
+import h5py
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from neuralib.util import util_h5py
+
+
+class GroupData(util_h5py.H5pyDataWrapper):
+    e: float = util_h5py.attr()
+
+
+class ReadData(util_h5py.H5pyDataWrapper):
+    a: int = util_h5py.attr()
+    b: int = util_h5py.attr()
+    c: float = util_h5py.attr()
+    d: np.ndarray = util_h5py.array()
+    g: GroupData = util_h5py.group()
+
+
+class TestH5pyDataWrapperRead(unittest.TestCase):
+    TMP_FILE = 'TestH5pyDataWrapperRead.h5'
+
+    @classmethod
+    def setUpClass(cls):
+        f = h5py.File(cls.TMP_FILE, 'w')
+        f.attrs['a'] = 1
+        f.attrs['b'] = 2
+        f.attrs['c'] = 3.141592653589793
+        f.create_dataset('d', data=np.arange(12))
+        g = f.create_group('g')
+        g.attrs['e'] = 2.718281828459045
+        f.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        Path(cls.TMP_FILE).unlink(missing_ok=True)
+
+    DATA: ReadData
+
+    def setUp(self):
+        self.DATA = ReadData(self.TMP_FILE)
+
+    def tearDown(self):
+        self.DATA = None
+
+    def test_attr(self):
+        self.assertEqual(self.DATA.a, 1)
+        self.assertEqual(self.DATA.b, 2)
+        self.assertAlmostEqual(self.DATA.c, math.pi)
+
+    def test_array(self):
+        self.assertEqual(self.DATA.d.ndim, 1)
+        self.assertEqual(self.DATA.d.shape, (12,))
+        assert_array_equal(self.DATA.d, np.arange(12))
+
+    def test_array_like(self):
+        self.assertEqual(self.DATA.d[2], np.arange(12)[2])
+        self.assertAlmostEqual(np.mean(self.DATA.d), np.mean(np.arange(12)))
+
+    def test_group(self):
+        g = self.DATA.g
+        self.assertIsInstance(g, GroupData)
+        self.assertAlmostEqual(g.e, math.e)
+
+    def test_set_attr(self):
+        with self.assertRaises(KeyError):
+            self.DATA.a = 0
+
+    def test_set_array(self):
+        with self.assertRaises(KeyError):
+            self.DATA.d = np.arange(24)
+
+
+class TestH5pyDataWrapperWrite(unittest.TestCase):
+    def test_write(self):
+        data = ReadData(TestH5pyDataWrapperRead.TMP_FILE, 'w')
+        data.a = 1
+        data.b = 2
+        data.c = 3.141592653589793
+        data.d = np.arange(12)
+        data.g.e = 2.718281828459045
+        data = None
+
+        main = TestH5pyDataWrapperRead()
+        main.DATA = ReadData(TestH5pyDataWrapperRead.TMP_FILE)
+        main.test_attr()
+        main.test_array()
+        main.test_array_like()
+        main.test_group()
+        main.test_set_attr()
+        main.test_set_array()
+
+
+if __name__ == '__main__':
+    unittest.main()
