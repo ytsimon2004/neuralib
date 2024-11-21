030d8c8d803b5f878d55856591f2e98a4491480d
Ta-Shun Su <antoniost29@gmail.com>
2024-11-21 12:33:18 +0100

    imglib.factory

--- src/neuralib/imglib/factory.py
+++ src/neuralib/imglib/factory.py
@@ -23,32 +23,39 @@ IMAGE_CHANNEL_TYPE = Literal['red', 'green', 'blue', 'r', 'g', 'b']
 @attrs.define(repr=False)
 class ImageProcFactory:
     """Factory for basic imaging processing"""
+
     image: np.ndarray
-    """image array. `Array[float, [H, W]|[H, W, 3]|[H, W, 4]]`"""
+    """image array. `Array[uint, [H, W]|[H, W, 3]|[H, W, 4]]`"""
+
+    mode: Literal['r', 'g', 'b', 'RGB', 'RGBA', 'gray']
 
     @classmethod
     def load(cls, file: PathLike,
-             to_rgba: bool | None = None,
-             to_rgb: bool | None = True) -> Self:
+             mode: Literal['RGB', 'RGBA', 'BGR'] = 'BGR',
+             alpha=False) -> Self:
         """
         Load the image file
 
         :param file: filepath of the image
-        :param to_rgba: convert to RGBA colorscale
-        :param to_rgb: convert to RGB colorscale
+        :param mode: source image color mode.
+        :param alpha: keep alpha channel
         :return:
         """
 
         img = cv2.imread(str(file))
 
-        if to_rgba:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
-            img = np.array(img, dtype=np.uint8)
+        if mode == 'BGR':
+            if alpha:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+                img = np.array(img, dtype=np.uint8)
+                mode = 'RGBA'
+            else:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                mode = 'RGB'
+        else:
+            raise RuntimeError('TODO')
 
-        if to_rgb:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        return ImageProcFactory(img)
+        return ImageProcFactory(img, mode)
 
     @property
     def height(self) -> int:
@@ -71,33 +78,41 @@ class ImageProcFactory:
 
         if channel in ('r', 'red'):
             img = cv2.split(self.image)[0]
+            mode = 'r'
         elif channel in ('g', 'green'):
             img = cv2.split(self.image)[1]
+            mode = 'g'
         elif channel in ('b', 'blue'):
             img = cv2.split(self.image)[2]
+            mode = 'b'
         else:
             raise KeyError(f'{channel}')
 
-        return attrs.evolve(self, image=img)
+        return attrs.evolve(self, image=img, mode=mode)
 
     def view_2d(self, flip: bool = True) -> Self:
-        """view `RGB` or `RGBA` image as 2d array. `Array[float, [H, W]]`"""
-        if self.image.shape[2] == 4:
+        """view `RGB` or `RGBA` image as 2d array. `Array[uint, [H, W]]`"""
+        if self.image.ndim == 2:
+            img = self.image
+
+        elif self.image.shape[2] == 4:
             w, h, _ = self.image.shape
             img = self.image.view(dtype=np.uint32).reshape((w, h))
+
         elif self.image.shape[2] == 3:
             r = self.image[:, :, 0]
             g = self.image[:, :, 1]
             b = self.image[:, :, 2]
             grayscale_image = 0.2989 * r + 0.5870 * g + 0.1140 * b
             img = grayscale_image.astype(np.uint8)
+
         else:
             raise RuntimeError(f'{self.image.shape} check')
 
         if flip:
             img = np.flipud(img)
 
-        return attrs.evolve(self, image=img)
+        return attrs.evolve(self, image=img, mode='gray')
 
     # ============= #
     # Basic Process #
@@ -106,13 +121,15 @@ class ImageProcFactory:
     def cvt_gray(self) -> Self:
         """convert to grayscale"""
         if self.image.ndim == 2:
-            return attrs.evolve(self, image=np.uint8(self.image))
+            img = np.uint8(self.image)
         elif self.image.shape[2] == 3:
-            return attrs.evolve(self, image=cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY))
+            img = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         elif self.image.shape[2] == 4:
-            return attrs.evolve(self, image=(cv2.cvtColor(self.image, cv2.COLOR_RGBA2GRAY)))
+            img = cv2.cvtColor(self.image, cv2.COLOR_RGBA2GRAY)
         else:
             raise RuntimeError('')
+
+        return attrs.evolve(self, image=img, mode='gray')
 
     def gaussian_blur(self, ksize: int, sigma: int) -> Self:
         img = cv2.GaussianBlur(self.image, (ksize, ksize), sigmaX=sigma, sigmaY=sigma)
